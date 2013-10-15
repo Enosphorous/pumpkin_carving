979a3fe15517e0a9ce73b3ba8a81823bbc6be708
Enosphorous <enosphorous@gmail.com>
2013-10-14 20:37:17 -0700

    Revert "Revert "Started on compatiblity with Biomes o' Plenty.""
    This reverts commit b9464eeacec9dbf56fe571f6aa3e308d9dbdbaf8.

--- enosphorous/pumpkins/item/ToolMaterial.java
+++ enosphorous/pumpkins/item/ToolMaterial.java
@@ -12,13 +12,23 @@ public class ToolMaterial {
 	public static EnumToolMaterial GOLD_CARVER;
 	public static EnumToolMaterial DIAMOND_CARVER;
 	
+	/**
+	 * Begin compatibility tool materials.
+	 */
+	
+	public static EnumToolMaterial MUD_CARVER;
+	public static EnumToolMaterial AMETHYST_CARVER;
+	
 	public static void init(){
 		
 		WOOD_CARVER = EnumHelper.addToolMaterial("WOOD_CARVER", 0, Remote.WOOD_DURABILITY, 1, 1, 0);
 		STONE_CARVER = EnumHelper.addToolMaterial("STONE_CARVER", 0, Remote.STONE_DURABILITY, 1, 3, 0);
 		IRON_CARVER = EnumHelper.addToolMaterial("IRON_CARVER", 0, Remote.IRON_DURABILITY, 1, 4, 0);
 		GOLD_CARVER = EnumHelper.addToolMaterial("GOLD_CARVER", 0, Remote.GOLD_DURABILITY, 1, 1, 0);
-		DIAMOND_CARVER = EnumHelper.addToolMaterial("IRON_CARVER", 0, Remote.DIAMOND_DURABILITY, 1, 6, 0);
+		DIAMOND_CARVER = EnumHelper.addToolMaterial("DIAMOND_CARVER", 0, Remote.DIAMOND_DURABILITY, 1, 6, 0);
+		
+		MUD_CARVER = EnumHelper.addToolMaterial("MUD_CARVER", 0, Remote.MUD_DURABILITY, 1, 1, 0);
+		AMETHYST_CARVER = EnumHelper.addToolMaterial("AMETHYST_CARVER", 0, Remote.AMETHYST_DURABILITY, 1, 7, 0);
 		
 	}
 
